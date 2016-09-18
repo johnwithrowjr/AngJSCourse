e3ec2490b401275aef0802e1af7922b5a1816c5f
John Withrow <john.withrow@gmail.com>
2016-09-17 22:35:11 -0600

    Version Dated 20160917

---
+++ assignment1/js/app.js
@@ -0,0 +1,29 @@
+var itemizeList = function(txtList)
+{
+  if (!txtList)
+  {
+    result=0;
+  }
+  else
+  {
+    var temp = txtList.split(";");
+    var result = temp.length;
+  };
+  return result;
+};
+
+var assess = function(numItems)
+{
+  if (numItems < 1)        { resp = "Please enter data first." }
+  else if (numItems > 3)   { resp = "Too much!" }
+  else                     { resp = "Looks good to me!" };
+  return resp;
+};
+
+angular.module('LunchChecker', [])
+.controller('LunchCheckerController', function($scope) {
+  $scope.checkHowMuch = function () {
+    var numberOfItems = itemizeList($scope.inputList);
+    $scope.response = assess(numberOfItems);
+  }
+});
